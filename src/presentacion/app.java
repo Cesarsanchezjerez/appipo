9b5bd6090841806611271faa039fa270c07f2f2d
PedroRamirezVerger <32263959+PedroRamirezVerger@users.noreply.github.com>
2017-12-27 18:51:13 +0100

    Creacion paneles

--- src/presentacion/app.java
+++ src/presentacion/app.java
@@ -3,13 +3,38 @@ package presentacion;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
-import javax.swing.JPanel;
+import javax.swing.BoxLayout;
 import java.awt.BorderLayout;
+import java.awt.ScrollPane;
+import java.awt.Panel;
+import javax.swing.JPanel;
+import javax.swing.JList;
+import javax.swing.JScrollPane;
+import javax.swing.JSplitPane;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import java.awt.Component;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import java.awt.FlowLayout;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import javax.swing.JMenuBar;
+import javax.swing.JSeparator;
+import java.awt.Color;
+import javax.swing.AbstractListModel;
+import net.miginfocom.swing.MigLayout;
+import javax.swing.border.TitledBorder;
 
-public class app {
+public class App {
 
 	private JFrame frame;
-	private JPanel panel;
+	private JButton btnAadirProyecto;
+	private JMenuBar menuBar;
+	private JList listaProyectos;
+	private JLabel lblArchivo;
+	private JPanel pnlProyecto;
 
 	/**
 	 * Launch the application.
@@ -18,7 +43,7 @@ public class app {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					app window = new app();
+					App window = new App();
 					window.frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -30,7 +55,7 @@ public class app {
 	/**
 	 * Create the application.
 	 */
-	public app() {
+	public App() {
 		initialize();
 	}
 
@@ -39,11 +64,45 @@ public class app {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(100, 100, 1003, 585);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(new MigLayout("", "[182.00px][697px,grow]", "[421px,grow][23px]"));
 		{
-			panel = new JPanel();
-			frame.getContentPane().add(panel, BorderLayout.CENTER);
+			listaProyectos = new JList();
+			listaProyectos.setBorder(new TitledBorder(null, "Lista de proyectos", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+			listaProyectos.setModel(new AbstractListModel() {
+				String[] values = new String[] {"Proyecto A", "Proyecto B", "Proyecto C"};
+				public int getSize() {
+					return values.length;
+				}
+				public Object getElementAt(int index) {
+					return values[index];
+				}
+			});
+			frame.getContentPane().add(listaProyectos, "cell 0 0,grow");
+		}
+		{
+			pnlProyecto = new JPanel();
+			pnlProyecto.setBorder(new TitledBorder(null, "Proyecto ", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+			frame.getContentPane().add(pnlProyecto, "cell 1 0 1 2,grow");
+			GridBagLayout gbl_pnlProyecto = new GridBagLayout();
+			gbl_pnlProyecto.columnWidths = new int[]{0};
+			gbl_pnlProyecto.rowHeights = new int[]{0};
+			gbl_pnlProyecto.columnWeights = new double[]{Double.MIN_VALUE};
+			gbl_pnlProyecto.rowWeights = new double[]{Double.MIN_VALUE};
+			pnlProyecto.setLayout(gbl_pnlProyecto);
+		}
+		{
+			btnAadirProyecto = new JButton("Añadir proyecto");
+			frame.getContentPane().add(btnAadirProyecto, "cell 0 1,alignx center,aligny center");
+		}
+		{
+			menuBar = new JMenuBar();
+			frame.setJMenuBar(menuBar);
+			{
+				lblArchivo = new JLabel("Archivo");
+				menuBar.add(lblArchivo);
+			}
 		}
 	}
 
--- src/presentacion/login.java
+++ src/presentacion/login.java
@@ -1,140 +1,71 @@
 package presentacion;
 
 import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.EventQueue;
+import java.awt.FlowLayout;
 
-import javax.swing.JFrame;
+import javax.swing.JButton;
+import javax.swing.JDialog;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
-import javax.swing.JLabel;
-import java.awt.GridBagLayout;
-import java.awt.GridBagConstraints;
-import java.awt.GridLayout;
-import java.awt.FlowLayout;
-import java.awt.Insets;
-import javax.swing.JTextField;
-import javax.swing.JButton;
-import javax.swing.JPasswordField;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JPasswordField;
 
-public class login extends JFrame {
-	private String contraseña="contraseña";
-	private JPanel contentPane;
-	private JPanel panellogin;
+public class Login extends JDialog {
+
+	private final JPanel contentPanel = new JPanel();
+	private JButton btnLogin;
 	private JLabel lblUsuario;
-	private JLabel lblContrasea;
-	private JTextField txtUsuario;
-	private JButton btnConectar;
-	private JPasswordField pwdContrasea;
-	private JLabel lblAviso;
+	private JLabel lblContraseña;
+	private JTextField textUsuario;
+	private JPasswordField passwordField;
 
 	/**
-	 * Launch the application.
+	 * Create the dialog.
 	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					login frame = new login();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-
-	/**
-	 * Create the frame.
-	 */
-	public login() {
-		setResizable(false);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 657, 425);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
-		setContentPane(contentPane);
+	public Login() {
+		setBounds(100, 100, 450, 300);
+		getContentPane().setLayout(null);
+		contentPanel.setBounds(0, 0, 434, 261);
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		getContentPane().add(contentPanel);
 		{
-			panellogin = new JPanel();
-			contentPane.add(panellogin, BorderLayout.CENTER);
-			GridBagLayout gbl_panellogin = new GridBagLayout();
-			gbl_panellogin.columnWidths = new int[] {0, 84, 0, 0, 89, 72, 0, 70, 98, 30, 0, 0};
-			gbl_panellogin.rowHeights = new int[] {109, 0, 0, 0, 0, 76, 0, 0, 0, 0, 62, 0, 0};
-			gbl_panellogin.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0};
-			gbl_panellogin.rowWeights = new double[]{0.0, 0.0, 0.0, Double.MIN_VALUE};
-			panellogin.setLayout(gbl_panellogin);
-			{
-				lblUsuario = new JLabel("usuario");
-				GridBagConstraints gbc_lblUsuario = new GridBagConstraints();
-				gbc_lblUsuario.anchor = GridBagConstraints.EAST;
-				gbc_lblUsuario.insets = new Insets(0, 0, 5, 5);
-				gbc_lblUsuario.gridx = 3;
-				gbc_lblUsuario.gridy = 1;
-				panellogin.add(lblUsuario, gbc_lblUsuario);
-			}
-			{
-				txtUsuario = new JTextField();
-				GridBagConstraints gbc_txtUsuario = new GridBagConstraints();
-				gbc_txtUsuario.insets = new Insets(0, 0, 5, 5);
-				gbc_txtUsuario.fill = GridBagConstraints.HORIZONTAL;
-				gbc_txtUsuario.gridx = 4;
-				gbc_txtUsuario.gridy = 1;
-				panellogin.add(txtUsuario, gbc_txtUsuario);
-				txtUsuario.setColumns(10);
-			}
-			{
-				lblContrasea = new JLabel("contraseña");
-				GridBagConstraints gbc_lblContrasea = new GridBagConstraints();
-				gbc_lblContrasea.anchor = GridBagConstraints.EAST;
-				gbc_lblContrasea.insets = new Insets(0, 0, 5, 5);
-				gbc_lblContrasea.gridx = 3;
-					gbc_lblContrasea.gridy = 2;
-				panellogin.add(lblContrasea, gbc_lblContrasea);
-			}
-			{
-				pwdContrasea = new JPasswordField();
-				GridBagConstraints gbc_pwdContrasea = new GridBagConstraints();
-				gbc_pwdContrasea.insets = new Insets(0, 0, 5, 5);
-				gbc_pwdContrasea.fill = GridBagConstraints.HORIZONTAL;
-				gbc_pwdContrasea.gridx = 4;
-				gbc_pwdContrasea.gridy = 2;
-				panellogin.add(pwdContrasea, gbc_pwdContrasea);
-			}
-			{
-				btnConectar = new JButton("conectar");
-				btnConectar.addActionListener(new BtnConectarActionListener());
-				GridBagConstraints gbc_btnConectar = new GridBagConstraints();
-				gbc_btnConectar.anchor = GridBagConstraints.NORTH;
-				gbc_btnConectar.insets = new Insets(0, 0, 5, 5);
-				gbc_btnConectar.gridx = 4;
-				gbc_btnConectar.gridy = 4;
-				panellogin.add(btnConectar, gbc_btnConectar);
-			}
-			{
-				lblAviso = new JLabel("");
-				GridBagConstraints gbc_lblAviso = new GridBagConstraints();
-				gbc_lblAviso.fill = GridBagConstraints.HORIZONTAL;
-				gbc_lblAviso.gridwidth = 4;
-				gbc_lblAviso.insets = new Insets(0, 0, 5, 5);
-				gbc_lblAviso.gridx = 1;
-				gbc_lblAviso.gridy = 9;
-				panellogin.add(lblAviso, gbc_lblAviso);
-			}
+			contentPanel.setLayout(null);
+		}
+		{
+			btnLogin = new JButton("Login");
+			btnLogin.addActionListener(new BtnLoginActionListener());
+			btnLogin.setBounds(93, 160, 228, 23);
+			contentPanel.add(btnLogin);
+		}
+		{
+			lblUsuario = new JLabel("Usuario");
+			lblUsuario.setBounds(93, 81, 46, 14);
+			contentPanel.add(lblUsuario);
+		}
+		{
+			lblContraseña = new JLabel("Contraseña");
+			lblContraseña.setBounds(93, 112, 56, 14);
+			contentPanel.add(lblContraseña);
+		}
+		{
+			textUsuario = new JTextField();
+			textUsuario.setBounds(159, 78, 162, 20);
+			contentPanel.add(textUsuario);
+			textUsuario.setColumns(10);
+		}
+		{
+			passwordField = new JPasswordField();
+			passwordField.setBounds(159, 109, 162, 20);
+			contentPanel.add(passwordField);
 		}
 	}
-
-	private class BtnConectarActionListener implements ActionListener {
-		public void actionPerformed(ActionEvent e) {
-			if (String.valueOf(pwdContrasea.getPassword()).equals(contraseña)) {
-				lblAviso.setBackground(Color.GREEN); lblAviso.setText("Contraseña correcta. Puede entrar"); lblAviso.setVisible(true); 
-				
-				} else {
-				lblAviso.setBackground(Color.RED); lblAviso.setText("Contraseña incorrecta. Vuelva a intentarlo"); lblAviso.setVisible(true);
-				
-				}
+	private class BtnLoginActionListener implements ActionListener {
+		public void actionPerformed(ActionEvent arg0) {
+			App.main(null);
 		}
+		
 	}
 }
