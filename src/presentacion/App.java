55675c981861669bbc68a9be2c2fe9a98ab92189
Cesarsanchezjerez <32263955+Cesarsanchezjerez@users.noreply.github.com>
2018-05-29 19:01:28 +0200

    quitar import de app
    eso de arriba

--- src/presentacion/App.java
+++ src/presentacion/App.java
@@ -6,12 +6,10 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.text.ParseException;
 import java.util.ArrayList;
 
 import javax.swing.AbstractListModel;
 import javax.swing.BoxLayout;
-import javax.swing.DefaultListModel;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
